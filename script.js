898d701bcd3aee6640e0d79330a8fd09f5f7ac35
Wellington Garcia <42919942+wellingtonag@users.noreply.github.com>
2024-07-30 17:29:00 -0300

    fiirst commit form test

---
+++ script.js
@@ -0,0 +1,4 @@
+// Link para o JavaScript do Bootstrap
+const script = document.createElement('script');
+script.src = 'https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js';
+document.body.appendChild(script);
